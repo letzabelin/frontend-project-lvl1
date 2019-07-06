644fe98bfebedf1f4ef1954769b780c9f7ac1243
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-07-06 21:18:51 +0300

    add game logic

---
+++ src/games/even-game.js
@@ -0,0 +1,13 @@
+import { getRandom, isEven } from '../extra';
+import { startGame } from '..';
+
+const rules = 'Answer "yes" if number even otherwise answer "no".';
+
+export const getEven = () => {
+  const question = getRandom(1, 100);
+  const correct = (isEven(question)) ? 'yes' : 'no';
+
+  return [correct, question];
+};
+
+export default () => startGame(rules, getEven);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import readlineSync from 'readline-sync';
 
-export const startWelcome = () => console.log('Welcome to the Brain Games!\n');
+export const displayGreetings = () => console.log('Welcome to the Brain Games!');
 
-export const helloUser = () => {
+export const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!\n`);
 
   return userName;
+};
+
+export const getAnswer = (question) => {
+  console.log(`Question: ${question}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  return answer;
+};
+
+export const startGame = (rules, dataQuestions) => {
+  // first greetings
+  displayGreetings();
+  console.log(`${rules}\n`);
+  const user = getUserName();
+
+  // start game
+  const askQuestion = (attempt = 1) => {
+    // good end
+    if (attempt > 3) {
+      console.log(`Congratulations, ${user}!`);
+      return null;
+    }
+
+    const q = dataQuestions();
+    const correctAnswer = q[0];
+    const question = q[1];
+    const userAnswer = getAnswer(question);
+
+    // bad end
+    if (correctAnswer !== userAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`);
+      console.log(`Let's try again, ${user}`);
+
+      return null;
+    }
+
+    // continuation of the game
+    console.log('Correct!');
+
+    return askQuestion(attempt + 1);
+  };
+
+  askQuestion();
 };
