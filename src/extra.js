8ee6f5cc945fffb4a3d78dccbdb7de19073fe6dc
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-07-07 21:54:25 +0300

    refactored gcd-function

--- src/extra.js
+++ src/extra.js
@@ -3,10 +3,4 @@ export const getRandom = (max, min) => Math.floor(Math.random() * (max - min) + 
 export const isEven = num => num % 2 === 0;
 
 // Greatest common divisor
-export const findGreatest = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-
-  return findGreatest(b, a % b);
-};
+export const findGreatest = (a, b) => ((b === 0) ? a : findGreatest(b, a % b));
