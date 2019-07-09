6fd11a6a8517671ec8f8fb2a2bc4e0efd283c96a
Andrew Zabelin <a.zabelin1@yandex.ru>
2019-07-09 20:10:12 +0300

    add progression-game

--- src/extra.js
+++ src/extra.js
@@ -4,3 +4,18 @@ export const isEven = num => num % 2 === 0;
 
 // Greatest common divisor
 export const findGreatest = (a, b) => ((b === 0) ? a : findGreatest(b, a % b));
+
+// Progression
+export const getProgression = (lenght, start, step) => {
+  let progression = '';
+
+  for (let i = start, j = 0; j < lenght; j += 1, i += step) {
+    if (j === lenght - 1) {
+      progression += `${i}`;
+    } else {
+      progression += `${i} `;
+    }
+  }
+
+  return progression.split(' ');
+};
--- src/games/progression.js
+++ src/games/progression.js
@@ -1,26 +1,20 @@
-import { getRandom } from '../extra';
+import { getRandom, getProgression } from '../extra';
 import { startGame } from '..';
 
 const rules = 'What number is missing in the progression?';
 
 const findNumber = () => {
-  let progression = '';
   const progressionLength = 10;
   const progressionStart = getRandom(1, 100);
   const progressionStep = getRandom(1, 6);
   const hiddenElement = getRandom(1, progressionLength);
 
-  for (let i = progressionStart, j = 0; j < progressionLength; j += 1, i += progressionStep) {
-    if (j === hiddenElement) {
-      progression += '.. ';
-    } else if (j === progressionLength - 1) {
-      progression += `${i}`;
-    } else {
-      progression += `${i} `;
-    }
-  }
+  const progressionArray = getProgression(progressionLength, progressionStart, progressionStep);
 
-  return progression;
+  const correct = `${progressionArray.splice(hiddenElement, 1, '..')}`;
+  const question = progressionArray.join(' ');
+
+  return [correct, question];
 };
 
 export default () => startGame(rules, findNumber);
